26453197c516d14dafdb0bf65db39b12c637bda3
Scott Cooper <cooperph@oregonstate.edu>
2018-01-28 20:47:13 -0600

    Added more basics to pages

--- awardhunter/src/App.js
+++ awardhunter/src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import UserPage from './components/UserPage/UserPage';
+import LoginPage from './components/LoginPage';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+    };
+
+    this.handleLogIn = this.handleLogIn.bind(this);
+  }
+
+  handleLogIn(e){
+    e.preventDefault();
+    console.log('button clicked!')
+    this.setState(prevState => ({
+      loggedIn: !prevState.loggedIn
+    }))
+  }
+
   render() {
+    let content = null;
+    if(this.state.loggedIn) {
+      content = <UserPage />
+    }
+    else {
+      content = <LoginPage onClick={this.handleLogIn}/>
+    }
+
     return (
-      <div className="App">
-        <p>AwardHunter Welcome Page!</p>
-        <p> changed for pull request</p>
+      <div className="App w3-theme-l3">
+        {content}
       </div>
     );
   }

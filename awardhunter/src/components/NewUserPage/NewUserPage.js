51cd00f070454ff5f196bd0273cf4a8a321737c4
Scott Cooper <cooperph@oregonstate.edu>
2018-03-15 01:23:45 -0500

    linked and added Nick's content to new user/ forgot password buttons on login page

--- awardhunter/src/components/NewUserPage/NewUserPage.js
+++ awardhunter/src/components/NewUserPage/NewUserPage.js
@@ -1,43 +1,72 @@
 import React from 'react';
 
 class NewUserPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+            formType: '1',
+            firstName: '',
+            lastName: '',
+            department: '',
+            email: '',
+            password: '',
+		};
+
+		this.onChange = this.onChange.bind(this);
+		this.onChange = this.onChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	
+	onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+	}
+	
+	handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state)
+	}
+	
     render() {
         return(
-            <div>
-                <h2>New User Page</h2>
-				<br />
-				<form>
-					<label>Username:
-						<input type="text" name="userName" id="userName" value size="30" />
-					</label>
-					<br />
-					<label>Password:
-						<input type="text" name="password" id="password" value size="30" />
-					</label>
-					<br />
-					<label>Email Address:
-						<input type="text" name="email" id="email" value size="30" />
-					</label>
-					<br />
-					<label>First name:
-						<input type='text' name='firstName' id='firstName' value size='30' placeholder='Joe'/>
-					</label>
-					<br />
-					<label>Last name:
-						<input type="text" name="lastName" id="lastName" value size="30" />
-					</label>
-					<br />
-					
-							<label> Upload Profile Picture: <input type="file" name="img" multiple />
-							</label>
-							<br />
-		
-					<input type='submit' value='Create New User' />
-				</form>
-
-
-			<button onClick={this.props.onClick} >Click ME!</button>   
-           		</div>
+			<div>
+				<div className="form">
+					<div className='form-group'>
+						<label>First Name: </label>
+						<input name='firstName' input='text' placeholder='First Name' value={this.state.firstName} onChange={this.onChange} />
+					</div>
+					<div className='form-group'>
+						<label>Last Name: </label>
+						<input name='lastName' input='text' placeholder='Last Name' value={this.state.lastName} onChange={this.onChange} />
+					</div>
+					<div className='form-group'>
+						<label>Department: </label>
+						<select name='department' value={this.state.department} onChange={this.onChange} >
+							<option value='10'>Choose Department</option>
+							<option value='1'>Accounting</option>
+							<option value='2'>Sales</option>
+							<option value='3'>Information Technololgy</option>
+							<option value='4'>Human Resources</option>
+							<option value='5'>Management</option>
+							<option value='6'>Customer Service</option>
+						</select>
+					</div>
+					<div className='form-group'>
+						<label>Email: </label>
+						<input name='email' type='email' placeholder='Email@email.com' value={this.state.email} onChange={this.onChange} />
+					</div>
+					<div className='form-group'>
+						<label>Password: </label>
+						<input name='password' type='password' placeholder='Password' value={this.state.password} onChange={this.onChange} />
+					</div>
+					<div className='form-group'>
+						<label>Account Type: </label>
+						<input name='accounttype' value={this.props.type} disabled/>
+					</div>
+				</div>
+				<button className='btn btn-primary' onClick={this.handleSubmit}>Click ME!</button>   
+			</div>
         )
     }
 }
